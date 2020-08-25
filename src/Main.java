e7d20e1911548382ab8eb152425ae2e187455f39
SienTien <hsientien0212@gmail.com>
2020-08-25 10:18:24 -0400

    with extension

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,20 @@ public class Main {
         boolean hasInsurance = false;
         double temp = 102.2;
         int numVisits = 5;
+        double copay = 23.00;
+        String nextVisit = "Sep 02 2019";
+        String contactNum = "2404993323";
 
         System.out.println("Patient Details:");
         System.out.println("Name: " + patientName);
+        System.out.println("Contact Number: " + contactNum);
         System.out.println("Patient has insurance: " + hasInsurance);
-        System.out.println("Body temprature: " + temp);
+        System.out.println("Copay: " + copay);
+        System.out.println("Body temperature: " + temp);
         System.out.println("Number of visits: " + numVisits);
+        System.out.println("Next visits: " + nextVisit);
+
+
 
 
     }
